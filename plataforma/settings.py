0d9ab70f6137026043f56a307ae6a3b781b2c230
JoseBritoo <brito7.jose@gmail.com>
2023-08-26 11:42:17 -0400

    Update settings.py

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "jnbds.lic21@uea.edu.br"
-EMAIL_HOST_PASSWORD = "jnbds2121"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
